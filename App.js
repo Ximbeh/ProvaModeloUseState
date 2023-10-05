128074e79c8b2bbf725500d3b6d50f66047e86e5
CAIRÉ DE MARCO MAIA <caire_maia@estudante.sesisenai.org.br>
2023-10-05 10:05:05 -0300

    nada suspeito

--- App.js
+++ App.js
@@ -1,11 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, TextInput, Button, FlatList, Text, StyleSheet } from 'react-native';
 
 export default function App() {
+  const [name, setName] = useState('');
+  const [nameList, setNameList] = useState([]);
+
+  const addName = () => {
+    if (name.trim() !== '') {
+      // Adicione o nome à lista
+      setNameList([...nameList, name]);
+      // Limpe o campo de entrada
+      setName('');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        placeholder="Digite um nome"
+        value={name}
+        onChangeText={(text) => setName(text)}
+      />
+      <Button title="Adicionar Nome" onPress={addName} />
+      <FlatList
+        data={nameList}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.listItem}>
+            <Text>{item}</Text>
+          </View>
+        )}
+      />
     </View>
   );
 }
@@ -13,8 +39,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 10,
+    paddingHorizontal: 8,
+  },
+  listItem: {
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderColor: 'gray',
   },
 });
